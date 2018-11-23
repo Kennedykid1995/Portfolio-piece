ac46f37bf901f0e096c42fb784e2ef7699bcd022
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-11-23 17:20:35 -0600

    major styling update
    been styling the nav, top and middle content.Just starting on the bottom of the Home page.

--- fic-client/src/App.js
+++ fic-client/src/App.js
@@ -3,62 +3,144 @@ import './App.css';
 import styled from 'styled-components'; 
 import{NavLink} from 'react-router-dom'; 
 
+//color palette
+//#7AA077 - green color
+//#B49071 - brown color
+//#00A7B1 - blue color
+//#FF8A00 , 255, 138, 0 - yellow/orange color
+//#FF0000 - red color
+
+
 const Site = styled.div`
   border: 1px solid black; 
   width: 100%; 
   height: auto; 
+  font-family: 'Roboto Slab', serif;
 `
 const NavBox = styled.div`
-  border: 1px solid blue;
   width: 100%;
   height: auto; 
   display: flex; 
-  justify-content: flex-end; 
+  justify-content: space-between; 
   align-items: center; 
+  padding: 10px 0;
+  background: #7AA077;
 `
 const Nav = styled.div`
-  border: 1px solid red; 
-  width: 50%; 
+  width: 60%; 
   height: auto; 
   padding: 10px 0;
   display: flex;
-  justify-content: space-evenly;  
+  margin-right: 10px;
+  justify-content: space-between;  
 `
 const NavItem = styled(NavLink)`
 font-size: 15px;
 text-decoration: none; 
 color: black;
-margin: 10px 20px;
+color: white;
 &:hover{
-    position: relative;
+  position: relative;
+  animation: pulse 2s infinite;
+}
+@keyframes pulse{
+  0%{color: white}
+  50%{color: #FF8A00}
+  100%{color: white}
 }
 :before {
-    content: "";
-    position: absolute;
-    width: 100%;
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: black;
-    visibility: hidden;
-    transform: scaleX(0);
-    transition: all 0.3s ease-in-out 0s;
-  }
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  bottom: 0;
+  left: 0;
+  background-color: #FF8A00;
+  visibility: hidden;
+  transform: scaleX(0);
+  transition: all 0.3s ease-in-out 0s;
+}
 :hover:before{
-    visibility: visible;
-    transform: scaleX(1);
-  }
+  visibility: visible;
+  transform: scaleX(1);
+}
+`
+const Logo = styled.div`
+  width: auto; 
+  height: auto; 
+  font-size: 25px;  
+  margin-left: 10px;
+  color: white;
 `
 const HomeBox = styled.div`
-  border: 1px solid green; 
   width: 100%; 
   height: auto;
+  padding-bottom: 80px; 
+`
+const HomeHeader = styled.img`
+  max-width: 100%; 
+  height: auto; 
+  clip-path: polygon(0 0, 100% 0, 100% 90%, 0 100%);
+`
+const HomeHeaderTextBox = styled.div`
+  width: 350px; 
+  height: auto;
+  font-size: 20px;  
+  border-bottom-left-radius: 45px;
+  border-bottom-right-radius: 45px;
+  border-top-right-radius: 45px;
+  background: rgba(255, 138, 0, 0.8);
+  color: white; 
+  padding: 10px;
+  position: absolute; 
+  left: 10%; 
+  top: 25%; 
+`
+
+const MiddleContentBox = styled.div`
+  width: 100%; 
+  height: auto; 
+  border: 1px solid lightgreen; 
+  margin: 60px 0;
+  display: flex; 
+  justify-content: space-evenly; 
+  align-items: center; 
+`
+
+const MiddleContentImg = styled.img`
+  width: 30%; 
+  height: auto; 
+  border: 30px solid #7AA077; 
+  border-bottom-left-radius: 45px;
+  border-bottom-right-radius: 45px;
+  border-top-left-radius: 45px;  
+`
+const MiddleTextBox = styled.div`
+  width: 40%; 
+  height: auto; 
+  display: flex; 
+  flex-direction: column;
+  align-items: center;
+  color: #B49071;
+`
+const BottomContentBox = styled.div`
+  width: 100%; 
+  height: auto; 
+  border: 1px solid blue; 
+`
+const BottomImg = styled.img`
+max-width: 100%; 
+height: auto; 
+clip-path: polygon(0 0, 100% 15%, 100% 84%, 0% 100%);
 `
 class App extends Component {
   render() {
     return (
       <Site>
         <NavBox>
+          <Logo>
+            Fresh Pickens
+          </Logo>
           <Nav>
             <NavItem to='/'>
             HOME
@@ -67,15 +149,38 @@ class App extends Component {
             MENU
             </NavItem>
             <NavItem to='/'>
-            CONTACT
+            REWARDS
             </NavItem>
             <NavItem to='/'>
-            LOCATION
+            LOCATIONS
+            </NavItem>
+            <NavItem to='/'>
+            CAREERS
             </NavItem>
           </Nav>
         </NavBox>
         <HomeBox>
+          <HomeHeader src="https://images.pexels.com/photos/533360/pexels-photo-533360.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <HomeHeaderTextBox>
+           <h1> Fresh Pickens<br />is farm to table yumminess</h1>
+           <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br />
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor<br />
+            in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in<br />
+            culpa qui officia deserunt mollit anim id est laborum.
+          </h3>
+          </HomeHeaderTextBox>
         </HomeBox>
+        <MiddleContentBox>
+          <MiddleContentImg src="https://images.pexels.com/photos/1358389/pexels-photo-1358389.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"/>
+          <MiddleTextBox>
+            <h1>New to the Table</h1>
+            <h2>Spinich Toast Bean Spread</h2>
+            <h3> This is the perfect breakfest for a cold day, you can use your rewards card to get double points while its in season.</h3>
+          </MiddleTextBox>
+        </MiddleContentBox>
+        <BottomContentBox>
+          <BottomImg src="https://images.pexels.com/photos/1002740/pexels-photo-1002740.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+        </BottomContentBox>
       </Site>
     );
   }
