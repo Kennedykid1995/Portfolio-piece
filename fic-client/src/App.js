04c438f0f9c6f6f46990abdcb011ea7ff361d642
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-11-23 20:39:33 -0600

    Home page 90% styled

--- fic-client/src/App.js
+++ fic-client/src/App.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 import{NavLink} from 'react-router-dom'; 
 
 //color palette
-//#7AA077 - green color
+//#0C391B - green color
 //#B49071 - brown color
 //#00A7B1 - blue color
 //#FF8A00 , 255, 138, 0 - yellow/orange color
@@ -24,7 +24,7 @@ const NavBox = styled.div`
   justify-content: space-between; 
   align-items: center; 
   padding: 10px 0;
-  background: #7AA077;
+  background: #0C391B;
 `
 const Nav = styled.div`
   width: 60%; 
@@ -100,7 +100,6 @@ const HomeHeaderTextBox = styled.div`
 const MiddleContentBox = styled.div`
   width: 100%; 
   height: auto; 
-  border: 1px solid lightgreen; 
   margin: 60px 0;
   display: flex; 
   justify-content: space-evenly; 
@@ -110,28 +109,57 @@ const MiddleContentBox = styled.div`
 const MiddleContentImg = styled.img`
   width: 30%; 
   height: auto; 
-  border: 30px solid #7AA077; 
-  border-bottom-left-radius: 45px;
-  border-bottom-right-radius: 45px;
-  border-top-left-radius: 45px;  
 `
 const MiddleTextBox = styled.div`
-  width: 40%; 
+  width: 30%; 
   height: auto; 
   display: flex; 
   flex-direction: column;
   align-items: center;
-  color: #B49071;
 `
 const BottomContentBox = styled.div`
   width: 100%; 
   height: auto; 
-  border: 1px solid blue; 
+  display: flex; 
+  justify-content: space-evenly; 
+  margin: 20px 0; 
 `
 const BottomImg = styled.img`
-max-width: 100%; 
-height: auto; 
-clip-path: polygon(0 0, 100% 15%, 100% 84%, 0% 100%);
+  max-width: 100%; 
+  height: auto; 
+  clip-path: polygon(0 0, 100% 15%, 100% 84%, 0% 100%);
+`
+const BottomTextBox = styled.div`
+  width: 30%; 
+  height: auto; 
+  display: flex; 
+  flex-direction: column;
+  align-items: center;
+`
+const BottomTextImg = styled.img`
+  width: 30%; 
+  height: 30%; 
+`
+const BottomSection = styled.div`
+  width: 100%; 
+  background: #604B38; 
+  height: auto; 
+  display: flex;
+  flex-wrap: wrap;  
+  justify-content: space-evenly; 
+`
+const InfoBox = styled.section`
+  width: 20%; 
+  height: 300px; 
+  color: #FF8A00; 
+`
+const TradeMark = styled.div`
+  width: 90%; 
+  height: auto; 
+  background:#0C391B; 
+  color: #FF8A00;
+  display: flex; 
+  justify-content: center; 
 `
 class App extends Component {
   render() {
@@ -178,9 +206,45 @@ class App extends Component {
             <h3> This is the perfect breakfest for a cold day, you can use your rewards card to get double points while its in season.</h3>
           </MiddleTextBox>
         </MiddleContentBox>
+        <BottomImg src="https://images.pexels.com/photos/1002740/pexels-photo-1002740.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
         <BottomContentBox>
-          <BottomImg src="https://images.pexels.com/photos/1002740/pexels-photo-1002740.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <BottomTextBox>
+            <h1>Right Ingredients</h1>
+            <h2> All of our ingredients come from local farmers,
+              and are fresh everyday. We want to display healthy
+              and local tastes. Each of our locations will have a 
+              signiture item that represents the local scene and 
+              what ingredients are available. 
+            </h2>
+          </BottomTextBox>
+          <BottomTextImg src="https://images.pexels.com/photos/256318/pexels-photo-256318.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <BottomTextBox>
+            <h1>Our Mission</h1>
+            <h2>We want to give our customers the most fresh and most healthy
+            meals that we can prepare. Also with ingredients that come straight from
+            local farms. We support local farmers and great food.
+            </h2>
+          </BottomTextBox>
         </BottomContentBox>
+        <BottomSection>
+          <InfoBox>
+            <h1>Contact</h1>
+            <p>123-456-7890</p>
+            <p>FreshPickens@gmail.com</p>
+          </InfoBox>
+          <InfoBox>
+            <h1>Find a Location</h1>
+            <p>Location Finder</p>
+          </InfoBox>
+          <InfoBox>
+            <h1>Careers</h1>
+            <p>Jobs</p>
+            <p>Future at Fresh Pickens</p>
+          </InfoBox>
+          <TradeMark>
+          <h3>2018 FRESH PICKENS.ALL RIGHTS RESERVED. PRIVACY POLICY</h3>
+        </TradeMark>
+        </BottomSection>
       </Site>
     );
   }
