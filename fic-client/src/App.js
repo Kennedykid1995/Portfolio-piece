49dfe2a44d452e4f0358ba0867e06c79bcb183fa
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-12-15 14:28:38 -0600

    working on the Rewards page

--- fic-client/src/App.js
+++ fic-client/src/App.js
@@ -25,11 +25,34 @@ const Site = styled.div`
 //comment out <Home /> and then work on the next page.
 //Repeat this until all the pages are styled 
 //then connect each of them via router.
-
 //Menu
 //Rewards
 //Locations
 //Careers
+const RewardsPage = styled.div`
+  width: 100%;
+  height: auto; 
+  display: flex; 
+  justify-content: center; 
+`
+const RewardsBox = styled.div`
+  width: 100%; 
+  height: auto; 
+`
+const DecorBox = styled.div`
+  width: 100%; 
+  height: 400px; 
+  clip-path: polygon(0 0, 100% 0, 100% 16%, 61% 32%, 0 57%);
+  background-color: #FF8A00;
+  position: absolute; 
+  bottom:114px; 
+  opacity: .7; 
+`
+const RewardsHeader = styled.img`
+  max-width: 100%; 
+  height: auto; 
+  clip-path: polygon(100% 0, 100% 84%, 59% 84%, 0 48%, 0 0);
+  `
 
 class App extends Component {
   constructor(){
@@ -43,16 +66,21 @@ class App extends Component {
       <Site>
         <NavBar />
         {/* <Home /> */}
-        <Route
+        {/* <Route
         exact
         path='/'
         render={props => (
           <MenuPage
           {...props}
           menu = {this.state.menu}
-          />
-        )}
-        />
+          /> )}
+        /> */}
+        <RewardsPage>
+          <RewardsBox>
+          <RewardsHeader src="https://images.pexels.com/photos/1580990/pexels-photo-1580990.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <DecorBox />
+          </RewardsBox>
+        </RewardsPage>
       </Site>
     );
   }
